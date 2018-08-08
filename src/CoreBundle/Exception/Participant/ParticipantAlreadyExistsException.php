b270902d18e45e47083caf8c27481ff5acd72bb0
Alexander Zuev <aaz@norse.digital>
2018-08-08 16:09:26 +0300

    Change error code in Extentions

--- src/CoreBundle/Exception/Participant/ParticipantAlreadyExistsException.php
+++ src/CoreBundle/Exception/Participant/ParticipantAlreadyExistsException.php
@@ -7,7 +7,7 @@ use Symfony\Component\HttpFoundation\Response;
 
 class ParticipantAlreadyExistsException extends \RuntimeException
 {
-    public function __construct($message = 'Participant already exists', $code = Response::HTTP_FORBIDDEN, Exception $previous = null)
+    public function __construct($message = 'Participant already exists', $code = Response::HTTP_BAD_REQUEST, Exception $previous = null)
     {
         parent::__construct($message, $code, $previous);
     }
